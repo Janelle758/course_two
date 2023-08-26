b1c71874c21664cd953200f07189cd77ae803b63
Janelle <your_email@example.com>
2023-08-25 22:13:48 -0400

    intial commit

---
+++ Janelle_Sargusingh.py
@@ -0,0 +1,5 @@
+first_name = "Janelle"
+middle_name = "Ebony"
+last_name = "Sargusingh"
+
+print(f"My full name is {first_name} {middle_name} {last_name}.")
---
+++ data_types.py
@@ -0,0 +1,17 @@
+# Task 3 - data_types.py
+
+# Variables illustrating different data types
+x = 3
+y = 4.5
+z = "Hello, World!"
+is_student = True
+fruits = ["apple", "banana", "orange"]
+person = {"name": "John", "age": 30, "is_student": True}
+
+# Printing the values and data types
+print(f"The value of x is {x}, and the data type of x is {type(x)}")
+print(f"The value of y is {y}, and the data type of y is {type(y)}")
+print(f"The value of z is {z}, and the data type of z is {type(z)}")
+print(f"The value of is_student is {is_student}, and the data type of is_student is {type(is_student)}")
+print(f"The value of fruits is {fruits}, and the data type of fruits is {type(fruits)}")
+print(f"The value of person is {person}, and the data type of person is {type(person)}")
